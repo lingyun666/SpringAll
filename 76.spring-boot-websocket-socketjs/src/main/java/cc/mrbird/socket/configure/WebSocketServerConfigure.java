7c2727be9528a5217187fdae2042ecfa14fbc3bd
“lingyun666” <873983080@qq.com>
2021-04-25 10:43:22 +0800

    Add websocket-socketis 添加代码注释

--- 76.spring-boot-websocket-socketjs/src/main/java/cc/mrbird/socket/configure/WebSocketServerConfigure.java
+++ 76.spring-boot-websocket-socketjs/src/main/java/cc/mrbird/socket/configure/WebSocketServerConfigure.java
@@ -11,14 +11,19 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
  * @author MrBird
  */
 @Configuration
-@EnableWebSocket
+@EnableWebSocket //开启WebSocket相关功能
 public class WebSocketServerConfigure implements WebSocketConfigurer {
 
+    //注入创建的MyStringWebSocketHandler，并将其注册到了WebSocketHandlerRegistry
     @Autowired
     private MyStringWebSocketHandler myStringWebSocketHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(myStringWebSocketHandler, "/connect").withSockJS();
+        registry
+                //当客户端通过/connecturl和服务端连接通信时，使用MyStringWebSocketHandler处理会话
+                .addHandler(myStringWebSocketHandler, "/connect")
+                //withSockJS的含义是，通信的客户端是通过SockJS实现的
+                .withSockJS();
     }
 }
--- 76.spring-boot-websocket-socketjs/src/main/java/cc/mrbird/socket/handler/MyStringWebSocketHandler.java
+++ 76.spring-boot-websocket-socketjs/src/main/java/cc/mrbird/socket/handler/MyStringWebSocketHandler.java
@@ -9,30 +9,37 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 /**
- * @author MrBird
+ * @author MrBird:  https://mrbird.cc/Spring-Boot%E6%95%B4%E5%90%88WebSocket.html
+ * 继承TextWebSocketHandler,并重写四个方法
+ * <p>
+ * webSocketSession对象代表每个客户端会话，包含许多实用方法
  */
 @Component
 public class MyStringWebSocketHandler extends TextWebSocketHandler {
 
     private Logger log = LoggerFactory.getLogger(this.getClass());
 
+    //1.和客户端链接成功的时候触发该方法；
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
         log.info("和客户端建立连接");
     }
 
+    //2.和客户端连接失败的时候触发该方法
     @Override
     public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
         session.close(CloseStatus.SERVER_ERROR);
         log.error("连接异常", exception);
     }
 
+    //3.和客户端断开连接的时候触发该方法
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         super.afterConnectionClosed(session, status);
         log.info("和客户端断开连接");
     }
 
+    //4.和客户端建立连接后，处理客户端发送的请求
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         // 获取到客户端发送过来的消息
